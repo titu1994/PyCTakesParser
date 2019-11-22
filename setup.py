4d5901f8aab1c02a029445e26f4923ea4e546041
Somshubra Majumdar <titu1994@gmail.com>
2019-11-21 23:00:37 -0600

    Correct config for setup.py

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ class UploadCommand(Command):
         try:
             self.status('Removing build artifacts...')
             rmtree(os.path.join(base_path, 'build'))
-            rmtree(os.path.join(base_path, '{}}.egg-info'.format(PACKAGE_NAME)))
+            rmtree(os.path.join(base_path, '{}.egg-info'.format(PACKAGE_NAME)))
         except OSError:
             pass
 
